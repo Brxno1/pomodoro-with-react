4e3aeb02b6cb40cc7d2df063b5814bd0b73b2cfc
Bruno Santos lameu <akiaura@akiaura>
2024-08-22 16:04:23 -0300

    Changing the maps to use Immer.js, seeking to make the code more readable when finding and changing cycle states

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer'
+
 import {
   ActionTypes,
   CreateNewCycleAction,
@@ -30,57 +32,50 @@ export type CyclesAction =
 
 export function CyclesReducer(state: CyclesState, action: CyclesAction): CyclesState {
   switch (action.type) {
-    case ActionTypes.CREATE_NEW_CYCLE:
-      // Quando a ação é do tipo 'CREATE_NEW_CYCLE', o reducer:
-      // 1. Cria uma cópia do estado atual usando o spread operator (...state)
-      // 2. Adiciona o novo ciclo à lista de ciclos, usando o spread operator ([...state.cycles])
-      // 3. Atualiza o ID do ciclo ativo com o ID do novo ciclo criado
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
+    case ActionTypes.CREATE_NEW_CYCLE: {
+      // Essa ação é responsável por criar um novo ciclo e adicioná-lo à lista de ciclos do estado.
+      // Ela também define o novo ciclo como o ciclo ativo.
+      return produce(state, (draftState) => {
+        draftState.cycles.push(action.payload.newCycle) // Adiciona o novo ciclo à lista de ciclos
+        draftState.activeCycleId = action.payload.newCycle.id // Define o novo ciclo como o ciclo ativo
+      })
+    }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      // Essa ação é responsável por marcar o ciclo ativo como concluído.
+      // Ela define o ciclo ativo como nulo e define a data de conclusão do ciclo ativo.
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      }) // Encontra o índice do ciclo ativo na lista de ciclos
+
+      if (currentCycleIndex < 0) {
+        return state
       }
 
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      // Quando a ação é do tipo 'MARK_CURRENT_CYCLE_AS_FINISHED', o reducer:
-      // 1. Cria uma cópia do estado atual usando o spread operator (...state)
-      // 2. Atualiza o ciclo ativo, definindo a propriedade 'finishedAt' com a data atual
-      // 3. Define o ID do ciclo ativo como null, indicando que não há mais um ciclo ativo
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return {
-              ...cycle,
-              finishedAt: new Date(),
-            }
-          }
-          return cycle
-        }),
-        activeCycleId: null,
-      }
+      return produce(state, (draftState) => {
+        draftState.activeCycleId = null // Define o ciclo ativo como nulo
+        draftState.cycles[currentCycleIndex].finishedAt = new Date() // Define a data de conclusão do ciclo ativo
+      })
+    }
 
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      // Quando a ação é do tipo 'INTERRUPT_CURRENT_CYCLE', o reducer:
-      // 1. Cria uma cópia do estado atual usando o spread operator (...state)
-      // 2. Atualiza o ciclo ativo, definindo a propriedade 'interruptedAt' com a data atual
-      // 3. Define o ID do ciclo ativo como null, indicando que não há mais um ciclo ativo
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return {
-              ...cycle,
-              interruptedAt: new Date(),
-            }
-          }
-          return cycle
-        }),
-        activeCycleId: null,
-      }
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      // Essa ação é responsável por interromper o ciclo ativo.
+      // Ela define o ciclo ativo como nulo e define a data de interrupção do ciclo ativo.
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      }) // Encontra o índice do ciclo ativo na lista de ciclos
 
-    default:
-      // Caso a ação não seja reconhecida, o reducer retorna o estado atual inalterado
+      if (currentCycleIndex < 0) {
+        return state
+      }
+      return produce(state, (draftState) => {
+        draftState.activeCycleId = null // Define o ciclo ativo como nulo
+        draftState.cycles[currentCycleIndex].interruptedAt = new Date() // Define a data de interrupção do ciclo ativo
+      })
+    }
+
+    default: {
       return state
+    }
   }
 }
